fd6e7aa8a97dc0d1d42368d1979b27023a7bc437
ofek gabay <ofek.gabay.he@gmail.com>
2020-06-05 16:07:00 +0300

    update

---
+++ Ofek Gabay/TicTacToe/logic.js
@@ -0,0 +1,16 @@
+let counter=2;
+let board =new Array(9);
+function addIcon() {
+    if (counter%2===0) {
+        event.target
+
+    }
+    else if (counter%2===!0) {
+        
+    }
+
+
+
+    counter++;
+    
+}
