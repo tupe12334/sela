c1948f0478d96b2ea9d44eb83c8235de92b0be17
tupe12334 <ofek.gabay.he@gmail.com>
2020-06-09 11:57:40 +0300

    update

---
+++ Ofek Gabay/Phone/myScript.js
@@ -0,0 +1,39 @@
+/*let body = document.getElementById("body");
+
+for (let index = 0; index < 8; index++) {
+    let button = document.createElement("index");
+    body.appendChild(button);
+}
+*/
+const phoneLine = document.getElementById("thePhoneNumberLine");
+const deletButton = document.getElementById("delet")
+deletButton.addEventListener("click",function () {
+    let theText = phoneLine.innerText;
+    if (theText.length==1) {
+        return null;
+    }
+    else{
+    
+        theText = theText.substring(0,theText.length-1);
+        
+    }
+    phoneLine.innerText = theText;
+})
+const clearButton = document.getElementById("clearB")
+clearButton.addEventListener("click",function(){
+    phoneLine.innerText ="0";
+})
+
+const phoneButton = document.getElementsByClassName("phoneButton");
+for (let index = 0; index < phoneButton.length; index++) {
+    phoneButton[index].addEventListener("click",function(){
+        console.log(this.value);
+        if (phoneLine.innerText.length>9) {
+            alert("to mach numbers!")
+        }
+        else{
+            phoneLine.textContent+=this.value;
+        }
+    })
+}
+
--- Ofek Gabay/myScripts.js
+++ Ofek Gabay/myScripts.js
@@ -1,3 +1,12 @@
-let obj1=new Object;
-obj1.name="ofek";
-          
+let obj1 = new Object;
+
+const inputElement = document.getElementById("name-Enter");
+
+const submitBo = document.getElementById("submit-B");
+submitBo.addEventListener("click", function () {
+    console.log("HI");
+    obj1.name = inputElement.value;
+    helloDiv.innerText = "hello: " + obj1.name;
+    alert("hello: "+obj1.name)
+});
+const helloDiv = document.getElementById("hello");
